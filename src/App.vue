4ae928316ad15fdec7b16809a354675c29c20159
Ruben Hernandez <hernandzruben9@gmail.com>
2025-01-26 22:22:55 -0500

    initial

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,25 @@
 <template>
-  <router-view></router-view>
+
+    <LoaderAnimation />
+    <Transition name="fade">
+      <router-view v-if="!isLoading"></router-view>
+    </Transition>
+
+
 </template>
 
-
 <script setup>
+import {ref, onMounted} from 'vue'
+import LoaderAnimation from "@/components/LoaderAnimation.vue";
+
+const isLoading = ref(true)
+
+onMounted(() => {
+  // Simular tiempo de carga
+  setTimeout(() => {
+    isLoading.value = false
+  }, 3000) // Ajusta este valor según tus necesidades
+})
 </script>
 
 <style>
@@ -11,5 +27,14 @@
 @import 'tailwindcss/components';
 @import 'tailwindcss/utilities';
 
-/* Additional global styles can be added here */
+/* Estilos adicionales para la transición */
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
 </style>
---
+++ src/components/LoaderAnimation.vue
@@ -0,0 +1,24 @@
+<template>
+  <div v-if="isLoading" class="fixed inset-0  bg-background-500 flex justify-center items-center z-50">
+    <DotLottieVue
+        class="w-72 h-72"
+        src="https://lottie.host/1a6f8f7f-752f-4178-b448-162564a50103/eFPKzFjyDK.lottie"
+        autoplay
+        loop
+    />
+  </div>
+</template>
+
+<script setup>
+import { DotLottieVue } from '@lottiefiles/dotlottie-vue'
+import { ref, onMounted } from 'vue'
+
+const isLoading = ref(true)
+
+onMounted(() => {
+  // Simular tiempo de carga
+  setTimeout(() => {
+    isLoading.value = false
+  }, 3000) // Ajusta este valor según tus necesidades
+})
+</script>
